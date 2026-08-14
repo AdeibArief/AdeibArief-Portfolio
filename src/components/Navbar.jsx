e85d291d84eb88ce067560e01228a49f06196dc0
AdeibArief <sadeib3@gmail.com>
2026-08-14 23:00:59 +0530

    Enhance Navbar shadow effect on scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
       className={cn(
         "fixed w-full z-40 transition-all duration-300 ",
         isScrolled
-          ? "py-3 bg-background/80 backdrop-blur-md shadow-xs"
+          ? "py-3 bg-background/80 backdrop-blur-lg shadow-xs"
           : "py-5",
       )}
     >
