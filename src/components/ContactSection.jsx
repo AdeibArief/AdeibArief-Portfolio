c8cb5b32f7efacc82159b4c0297802364c0ba7a8
AdeibArief <sadeib3@gmail.com>
2026-08-14 17:28:30 +0530

    Added new projects and skills

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -74,7 +74,7 @@ const ContactSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
           <div className="space-y-8">
             <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
-            <div className="space-y-6 justify-center">
+            <div className="space-y-6">
               <div className="flex items-center space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
                   <Mail className="size-6 text-primary" />
